3b39d0ff23514c06ea60ca27a079ea59210004a3
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2022-08-06 15:53:29 +0800

    first try

---
+++ sr/entrypoint.sh
@@ -0,0 +1,290 @@
+#!/bin/sh
+
+help()
+{
+    echo "Usage: TODO"
+    exit 2
+}
+
+SHORT=db:,webip:,webport:,weburlssl:,gameip:,gameport:,msgwc,mailmsg:,dlres:,lang:,loginpass:,po:,nmsv:,nmow:,nmrg:,ssl:,h
+LONG=datebase:,web_ip:,web_port:,web_url_ssl:,game_ip:,game_port:,message_welcome:,mail_message:,download_resource:,language:,login_password:,player_online:,name_server:,name_owner:,name_region:,ssl:,help
+OPTS=$(getopt -a -n dockersr --options $SHORT --longoptions $LONG -- "$@")
+
+VALID_ARGUMENTS=$# # Returns the count of arguments that are in short or long options
+
+if [ "$VALID_ARGUMENTS" -eq 0 ]; then
+  help
+fi
+
+eval set -- "$OPTS"
+
+while :
+do
+  case "$1" in
+    # Server Datebase & Ip Config
+    -db | --datebase )
+      set_datebase="$2"
+      shift 2
+      ;;
+    -webip | --web_ip )
+      set_web_ip="$2"
+      shift 2
+      ;;
+    -webport | --web_port )
+      set_web_port="$2"
+      shift 2
+      ;;
+    -weburlssl | --web_url_ssl )
+      set_web_url_ssl="$2"
+      shift 2
+      ;;
+    -ssl | --ssl )
+      set_ssl="$2"
+      shift 2
+      ;;
+    -gameip | --game_ip )
+      set_game_ip="$2"
+      shift 2
+      ;;
+    -gameport | --game_port )
+      set_game_port="$2"
+      shift 2
+      ;;
+    # Server Setup
+    -dlres | --download_resource )
+      set_download_resource="$2"
+      shift 2
+      ;;
+    -lang | --language )
+      set_language="$2"
+      shift 2
+      ;;
+    -loginpass | --login_password )
+      set_login_password="$2"
+      shift 2
+      ;;
+    -po | --player_online )
+      set_player_online="$2"
+      shift 2
+      ;;
+    # Owner Server
+    -nmsv | --name_server )
+      set_name_server="$2"
+      shift 2
+      ;;
+    -nmow | --name_owner )
+      set_name_owner="$2"
+      shift 2
+      ;;
+    -nmrg | --name_region )
+      set_name_region="$2"
+      shift 2
+      ;;
+    # Message Server
+    -msgwc | --message_welcome )
+      set_message_welcome="$2"
+      shift 2
+      ;;
+    # Mail Message
+    -mailmsg | --mail_message )
+      set_mail_message="$2"
+      shift 2
+      ;;
+    # Help
+    -h | --help)
+      help
+      ;;
+    --)
+      shift;
+      break
+      ;;
+    *)
+      echo "Unexpected option: $1"
+      help
+      ;;
+  esac
+done
+
+folder_gc="/home/dockersr"
+folder_resources="$folder_gc/data"
+update=false
+timems=date
+
+cd $folder_gc
+
+OSVS=$(. /etc/os-release && printf '%s\n' "$NAME")
+SUB="Alpine"
+version=$(cat VERSION)
+version_res="main"
+
+echo $version
+
+# Switch RS
+if echo "$version" | grep -q "2.6"; then
+ echo "Use res 2.6"
+ version_res="2.6"
+elif echo "$version" | grep -q "2.7"; then
+ echo "Use res 2.7"
+ version_res="2.7"
+elif echo "$version" | grep -q "2.8"; then
+ echo "Use res 2.8"
+ version_res="2.8"
+fi
+
+# Time Server
+if [ "$OSVS" = "Ubuntu" ]; then
+  mv /etc/localtime /etc/localtime-old
+  ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+fi
+
+echo "This system run with OS $OSVS"
+
+# Building Data Source and Generated Resources
+if [ -d "$folder_resources/BinOutput" ] 
+then
+    ls $folder_resources
+    echo "Resources folder already exists..."
+else
+    echo "Update?"
+    update=true
+fi
+
+# Foce Mode
+if [ "$set_download_resource" = "yes" ]; then
+     echo "Update"
+     update=true
+fi
+if [ "$set_download_resource" = "no" ]; then
+     echo "skip"
+     update=false
+fi
+
+if [ ! -f "config.json" ]; then
+
+ # If there is no config file
+ echo "No config file was found, try to use from command file"
+
+ # languages
+ if [ -z "$set_language" ]; then
+  set_language="en_US"
+ fi
+
+ # online
+ if [ -z "$set_player_online" ]; then
+  set_player_online="-1"
+ fi
+
+ # Nama Server
+ if [ -z "$set_name_server" ]; then
+  set_name_server="YuukiPS"
+ fi
+
+ # Nama Owner
+ if [ -z "$set_name_owner" ]; then
+  set_name_owner="Ayaka"
+ fi
+
+ # Region
+ if [ -z "$set_name_region" ]; then
+  set_name_region="localhost"
+ fi
+
+ # SSL
+ if [ -z "$set_ssl" ]; then
+  set_ssl="false"
+ fi
+
+ # URL SSL
+ if [ -z "$web_url_ssl" ]; then
+  web_url_ssl="false"
+ fi
+
+ # if no config just boot
+ npm start
+
+ # ip/domain public for web (Outside docker)
+ if [ -z "$set_web_ip" ]; then
+  set_web_ip=0.0.0.0  
+ fi
+
+ # Ip web port
+ if [ -z "$set_web_port" ]; then
+  set_web_port="443"
+ fi
+
+ echo "Server Web Public: $set_web_ip:$set_web_port"
+ echo "URL SSL Web Public: $web_url_ssl"
+
+ # ip public for game (Outside docker)
+ if [ -z "$set_game_ip" ]; then
+  set_game_ip=$set_web_ip
+ fi 
+
+ # Ip game port
+ if [ -z "$set_game_port" ]; then
+  set_game_port="22102"
+ fi
+
+ echo "Server Ip Game: $set_game_ip:$set_game_port"
+
+ # Welcome message
+ if [ -z "$set_message_welcome" ]; then
+  set_message_welcome="Welcome to $set_name_server\n\nUse !help for help\n\nRegion: $set_name_region\nCommunity: discord.yuuki.me"
+ fi
+
+ # Send Email When Registration
+ if [ -z "$set_mail_message" ]; then
+  set_mail_message="Hi, Thank you for registering on Yuuki Server, as a sign of gratitude for you we give gifts, you can also get more gifts with /give all on console, please visit us for help and type !help for more info.<type=\"browser\" text=\"Discord\" href=\"https://discord.yuuki.me\"/>"
+ fi
+
+ # Need database (Outside docker)
+ if [ -z "$set_datebase" ]; then
+  echo "No Datebase? exit!"
+  exit 1
+ else      
+  echo "Server MongoDB: $set_datebase"
+  json -q -I -f config.json -e "this.MONGO_URI='$set_datebase'"
+  json -q -I -f config.json -e "this.MONGO_URI='$set_datebase'"
+ fi
+
+ # Config IP Game
+ #json -q -I -f config.json -e "this.GAMESERVER.SERVER_IP='$set_game_ip'"
+ #json -q -I -f config.json -e "this.GAMESERVER.SERVER_PORT='$set_game_port'" 
+
+ # Config Game Web
+ #json -q -I -f config.json -e "this.HTTP.HTTP_HOST='$set_web_ip'"
+ #json -q -I -f config.json -e "this.HTTP.HTTP_PORT='$set_web_port'"
+
+ # SSL
+ #json -q -I -f config.json -e "this.server.http.encryption.useEncryption='$set_ssl'"
+ #json -q -I -f config.json -e "this.server.http.encryption.useInRouting='$set_web_url_ssl'"
+
+ # Config Language Server
+ #json -q -I -f config.json -e "this.language.language='$set_language'"
+
+ # Config max number of player online
+ #json -q -I -f config.json -e "this.account.maxPlayer='$set_player_online'"
+ 
+ # Config password
+ #json -q -I -f config.json -e "this.account.EXPERIMENTAL_RealPassword='$set_login_password'" 
+
+ # Config Welcome Message
+ #json -q -I -f config.json -e "this.server.game.joinOptions.welcomeMessage='$set_message_welcome'"
+
+ # Config Welcome Email
+ #json -q -I -f config.json -e "this.server.game.joinOptions.welcomeMail.title='Welcome to $set_name_server'"
+ #json -q -I -f config.json -e "this.server.game.joinOptions.welcomeMail.sender='$set_name_owner'"
+ #json -q -I -f config.json -e "this.server.game.joinOptions.welcomeMail.content='$set_mail_message'"
+
+ # Config Console
+ #json -q -I -f config.json -e "this.server.game.serverAccount.signature='Console $set_name_server ($set_name_region)'"
+ #json -q -I -f config.json -e "this.server.game.serverAccount.nickName='$set_name_owner'"
+
+else
+ # If found config file (maybe restart?)
+ echo "Found config files, ignore from command"
+ rm -R -f logs/*
+fi
+
+# Game Server
+npm start
---
+++ sr/run.sh
@@ -0,0 +1,280 @@
+#!/bin/bash
+runit="local"
+product="dockersr"
+os=$1
+metode=$2
+usebranch=$3
+
+filejson="work/config.json" 
+filejson_res="todo/config.backup"
+
+switchbc="Patch-1.0"
+switcres="1.0"
+
+version_gchash="unknown";
+version_rshash="unknown";
+
+# Clone Data
+if [ "$os" = "data" ];then
+
+ echo "Start clone data..."
+
+ if [ "$metode" = "core" ];then
+  echo "~ Get Core"
+  git clone https://github.com/akbaryahya/YuukiSR YuukiSR
+ fi
+ if [ "$metode" = "res" ];then
+  echo "~ Get Resources"
+  git clone https://gitlab.com/yukiz/SR_Resources SR_Resources
+ fi
+ echo "EXIT NOW"
+ exit 1
+fi
+
+# Get Data if not found
+if [ ! -d "SR_Resources" ]; then  
+  echo "No Found Resources, let's clone first"
+  sh run.sh data res
+fi
+
+# OS
+if [ -z "$os" ]; then
+ os="local"
+fi
+
+# Metode
+if [ -z "$metode" ]; then
+ metode="build"
+fi
+
+# Branch Switch Version
+if [ "$usebranch" = "0" ];then
+ switchbc="Patch-1.0"
+ switcres="1.0"
+fi
+
+echo OS: $os - Metode: $metode - Branch:$switchbc
+
+# Check SR
+cd YuukiSR
+
+# Switch
+#ls -a
+branch_now=$(git rev-parse --abbrev-ref HEAD)
+if [ -z "$branch_now" ]; then
+ echo "Error get name branch"
+ exit 1
+fi
+# if HEAD
+if [ "$branch_now" = "HEAD" ];then
+ echo "This seems to work on GitHub Action, or first time? so let's switch to original to check version";
+ # $branch_now = $switchbc
+fi
+if [ "$switchbc" != "$branch_now" ]; then
+ echo "Switch $branch_now to $switchbc"
+ git switch $switchbc
+else
+ echo "You're already there $branch_now"
+fi
+# Get Hash
+version_gchash=$(git rev-parse --short=7 HEAD)
+if [ -z "$version_gchash" ]; then
+ echo "Error get hash"
+ exit 1
+fi
+# Back to home
+cd ..
+
+# Check Resources (Only if need with metode res)
+if [ "$metode" = "res" ];then
+ cd SR_Resources
+ branch_res_now=$(git rev-parse --abbrev-ref HEAD)
+ if [ -z "$branch_res_now" ]; then
+  echo "Error get name branch resources"
+  exit 1
+ fi
+ # if HEAD
+ if [ "$branch_res_now" = "HEAD" ];then
+  echo "This seems to work on GitHub Action, or first time? so let's switch to original to check version";
+  # $branch_res_now = $switcres
+ fi
+ if [ "$switcres" != "$branch_res_now" ]; then
+  echo "Switch Resources $branch_res_now to $switcres"
+  git switch $switcres
+ else
+  echo "You're already there resources $branch_res_now"
+ fi
+ # Get Hash GC
+ version_rshash=$(git rev-parse --short=7 HEAD)
+ if [ -z "$version_rshash" ]; then
+  echo "Error Get Hash Resources"
+  exit 1
+ fi
+ # Back to home
+ cd ..
+fi
+
+# Copy Hash
+echo -n "$version_gchash" > VERSION_SR_$switchbc
+if [ "$metode" = "res" ];then
+ echo -n "$version_rshash" > VERSION_RS_$switcres
+fi
+
+# Copy TMP version
+version_last_commit=$os-$switchbc-$version_gchash
+version_last_sw=$os-$switchbc
+echo $version_last_commit
+echo -n "$version_last_commit" > VERSION_TMP
+
+if [ ! -d "work" ]; then
+ echo "Make folder work.."
+ mkdir -p work
+fi
+if [ ! -d "todo" ]; then
+ echo "Make folder todo.."
+ mkdir -p todo
+fi
+
+if [ "$metode" = "start" ];then
+
+ if [ "$os" = "local" ];then
+
+  echo "Not yet available for local, because regular folders are complicated"
+  exit 1
+
+ else
+  ip=$4
+  ipdb=$5
+  res=$6  
+  if [ -z "$ip" ]; then
+   ip="127.0.0.1"
+  fi
+  if [ -z "$ipdb" ]; then
+   ipdb="$ip:27017"
+  fi
+  if [ -z "$res" ]; then
+   res="resources_sr_$switchbc"
+  fi
+  echo "Start Docker with IP $ip"
+  docker run \
+  --rm -it \
+  -v $res:/home/YuukiSR/data \
+  -p 22102:22102/udp \
+  -p 443:443/tcp \
+  -p 80:80/tcp \
+  siakbary/$product:$version_last_commit \
+  --datebase "mongodb://$ipdb" \
+  --web_ip "$ip" \
+  --game_ip "$ip" \
+  --ssl "false" \
+  --web_url_ssl "false" \
+  --login_password "true"
+ fi
+
+fi
+
+# if clean
+if [ "$metode" = "clean_work" ];then
+ rm -R -f work/*
+ rm -R -f .gradle/*
+ rm -R -f bin/*
+fi
+
+# if sync
+if [ "$metode" = "sync" ];then
+ cd YuukiSR
+ whosm=$4
+ getme=$5
+ dlrepo=$6
+ if [ -z "$dlrepo" ]; then
+  dlrepo="CrepeSR"
+ fi
+ if [ -z "$whosm" ]; then
+  whosm="Crepe-Inc"
+ fi
+ if [ -z "$getme" ]; then
+  if [ "$switchbc" = "Patch-2.8" ];then
+   getme="development"
+  elif [ "$switchbc" = "Patch-2.8-Early" ];then
+   getme="development"
+   # git rebase --ignore-whitespace
+  else
+   getme="2.6"
+  fi
+ fi
+ git pull https://github.com/$whosm/$dlrepo.git $getme
+ cd ..
+fi
+
+if [ "$metode" = "sync_raw" ];then
+ cd YuukiSR
+ whosm=$4
+ getme=$5
+ git pull $whosm $getme
+ cd ..
+fi
+
+# if build
+if [ "$metode" = "build" ];then
+ 
+ # if localhost
+ if [ "$os" = "local" ];then    
+
+  # Windows User:
+  # https://stackoverflow.com/a/49584404 & https://stackoverflow.com/a/64272135  
+
+  # Remove file
+  we_clean_it=$4
+  if [ "$we_clean_it" = "clean" ];then   
+   if test -f "$filejson"; then
+    echo "Found file config.json"
+    cp -rTf $filejson $filejson_res
+   fi
+   echo "Remove file work (ending)"   
+   rm -R -f work/*
+  fi
+
+  echo "Start Bulid..."
+  cd YuukiSR
+
+  echo "Update lib stuff"
+  npm update
+
+  # Back to home directory
+  cd .. 
+
+  echo "Copy file version local"
+  cp -rTf VERSION_TMP work/VERSION
+
+ else
+  # build local
+  # sh run.sh local build $usebranch $4
+
+  # Version Docker
+  echo "Copy file version docker"  
+  echo -n "$version_last_commit" > work/VERSION
+
+  # Bulid Local
+  docker build -t "$product:$version_last_commit" -f os_$os .;
+  # Tag to multi source
+  echo "Add image to repo public"  
+  docker tag "$product:$version_last_commit" "siakbary/$product:$version_last_commit"
+  docker tag "$product:$version_last_commit" "siakbary/$product:$version_last_sw"
+  # Private Repo
+  echo "Add image to private repo"  
+  docker tag "$product:$version_last_commit" "repo.yuuki.me/$product:$version_last_commit"
+ fi
+ 
+fi
+
+# Push Public
+if [ "$metode" = "push" ];then
+ docker push siakbary/$product:$version_last_commit
+ docker push siakbary/$product:$version_last_sw
+fi
+
+# Push Private
+if [ "$metode" = "private_push" ];then
+ echo "push private"  
+ docker push repo.yuuki.me/$product:$version_last_commit
+fi
